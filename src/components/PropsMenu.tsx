424f350991d9584d0f8800f641e5b014f3ee0fe0
ali <ali.atwa2040@gmail.com>
2023-04-01 19:18:57 +0200

    menu style

--- src/components/PropsMenu.tsx
+++ src/components/PropsMenu.tsx
@@ -20,8 +20,9 @@ const PropsMenu: React.FC<propsMenu | any> = ({propsMenu}) => {
 <div className="flex flex-col  px-4 space-y-3 mx-2">
 
 <div className="flex h-8 items-center">
-  <div className="basis-36 shrink-0 flex items-center space-x-2">
-    <span>Rotation</span><a></a></div>
+  <div className="basis-36 shrink-0 flex items-center space-x-2  text-sm">
+    <span>Rotation</span>
+    </div>
     <div className="relative flex items-center w-full text-neutral-800 bg-white border-neutral-200 border border-solid overflow-hidden px-1.5 h-8 rounded-md">
   <input className="placeholder-neutral-300 outline-none font-input w-full" role="input" type="number" max="360" min="0" value={propsMenu.rotation}/></div></div>
 
@@ -32,10 +33,21 @@ const PropsMenu: React.FC<propsMenu | any> = ({propsMenu}) => {
 </div>
 
 <div className="flex h-8 items-center">
-  <label className="basis-40 flex items-center space-x-2">
-    <span>Color</span><a></a></label>
+  <label className="basis-40 flex items-center space-x-2 text-sm">
+    <span>Color</span>
+    </label>
     <div className="h-6 w-6 border border-solid border-neutral-75 rounded-full shadow-xs hover:cursor-pointer hover:shadow-sm transition-shadow ml-auto"></div>
     </div>
+ 
+
+    <div className="flex h-8 items-center font-normal">
+<div className="shrink-0 flex grow items-center space-x-2  text-sm">
+    <span>Use Img</span>
+    </div>
+    <label className="relative inline-flex items-center cursor-pointer">
+  <input type="checkbox" value="" className="sr-only peer"/>
+  <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+</label></div>
 
     <div className="flex flex-col font-normal">
   <label className="mb-2 text-sm" htmlFor="img">Image</label>
@@ -43,17 +55,26 @@ const PropsMenu: React.FC<propsMenu | any> = ({propsMenu}) => {
   <input className="placeholder-neutral-300 select-none font-input w-full" role="input" name="img" value={propsMenu.img}/></div>
 </div>
 
-<div className="flex flex-col font-normal">
-  <label className="mb-2 text-sm" htmlFor="xpos">XPos</label>
-  <div className="relative flex items-center w-full text-neutral-800  disabled:opacity-75 border-neutral-100 border border-solid overflow-hidden px-3 h-8 rounded-md bg-gray-50 border-error-default">
-  <input className="placeholder-neutral-300 select-none  font-input w-full" disabled role="input" name="xpos" value={propsMenu.XPos}/></div>
-</div>
 
-<div className="flex flex-col font-normal">
-  <label className="mb-2 text-sm" htmlFor="xpos">YPos</label>
-  <div className="relative flex items-center w-full text-neutral-800  disabled:opacity-75 border-neutral-100 border border-solid overflow-hidden px-3 h-8 rounded-md bg-gray-50 border-error-default">
-  <input className="placeholder-neutral-300 select-none font-input w-full" disabled role="input" name="zpos" value={propsMenu.ZPos}/></div>
-</div>
+
+<div className="flex h-8 items-center font-normal">
+<div className="basis-1/2 shrink-0 flex items-center space-x-2 text-sm">
+    <span>XPos</span>
+    </div>
+  <div className="relative flex items-center w-full text-neutral-800 disabled:opacity-75 border-neutral-100 border border-solid overflow-hidden px-3 h-8 rounded-md bg-gray-50 ">
+  <input className="placeholder-neutral-300 select-none font-input w-full" disabled role="input" name="xpos" value={propsMenu.XPos}/>
+  </div>
+  </div>
+
+  <div className="flex h-8 items-center font-normal">
+<div className="basis-1/2 shrink-0 flex items-center space-x-2 text-sm">
+    <span>YPos</span>
+    </div>
+  <div className="relative flex items-center w-full text-neutral-800 disabled:opacity-75 border-neutral-100 border border-solid overflow-hidden px-3 h-8 rounded-md bg-gray-50 ">
+  <input className="placeholder-neutral-300 select-none font-input w-full" disabled role="input" name="zpos" value={propsMenu.ZPos}/>
+  </div>
+  </div>
+
 
 </div>
         </div>
